dda85e87c187ea7cff86a5cd38d2746160d929b2
pandya.ameya <pandya.ameya@gmail.com>
2017-09-04 14:10:38 +0530

    Add project files.

---
+++ Mitsubishi Project/Form1.cs
@@ -0,0 +1,21 @@
+﻿using Creek.UI.Metro.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mitsubishi_Project
+{
+    public partial class Welcome_Form : MetroForm
+    {
+        public Welcome_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
